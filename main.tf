ad2f74af9922b8bc95a3a6537bf9a92ba05c7f0b
Chella S <2chellaa@gmail.com>
2023-04-06 11:56:21 +0530

    Org name updated again

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "Chellas-Tech-Park "
+    organization = "Chellas-Tech-Park"
 
     workspaces {
       name = "learn-terraform-github-actions"
